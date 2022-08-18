e9cb299d59bd99ea00033cec7926530e356b39c3
root <root@ip-172-31-23-130.us-east-2.compute.internal>
2022-08-18 13:31:57 +0000

    shell project

---
+++ src/main/java/com/shell/App.java
@@ -0,0 +1,13 @@
+package com.shell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
